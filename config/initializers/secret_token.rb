68f5c0c9156d997e9ff52b4d31c7a4c281359fcc
Zach Latta <zchlatta@gmail.com>
2013-06-16 01:01:40 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ferret::Application.config.secret_key_base = 'f597115d8553c7a153d2f73d41b405120dc5ef9f209b79899e0c3ecf30f6b763e8d493abd6f446fcedd08403fa0d213057131426d085307f1f57334c2e93b3e5'
